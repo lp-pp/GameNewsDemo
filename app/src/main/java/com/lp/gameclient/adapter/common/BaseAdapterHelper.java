2c8397ebff63aa6c928d53d91c698987ccc0fa69
1049345904@qq.com <lp1049345904>
2017-08-29 22:11:19 +0800

    update

--- app/src/main/java/com/lp/gameclient/adapter/common/BaseAdapterHelper.java
+++ app/src/main/java/com/lp/gameclient/adapter/common/BaseAdapterHelper.java
@@ -1,5 +1,8 @@
 package com.lp.gameclient.adapter.common;
 
+import android.os.Build;
+import android.support.annotation.ColorInt;
+import android.support.annotation.ColorRes;
 import android.support.annotation.DrawableRes;
 import android.support.annotation.StringRes;
 import android.util.SparseArray;
@@ -9,49 +12,80 @@ import android.widget.TextView;
 
 import com.lp.gameclient.utils.GlideUtil;
 
+import static android.R.attr.textColor;
+
 /**
  * Created by LP on 2017/8/22/20:05.
  */
 
-public abstract class BaseAdapterHelper<T>{
+public abstract class BaseAdapterHelper<T> {
     protected SparseArray<View> views;
 
     public abstract View getItemView();
 
     private <V extends View> V retrieveView(int viewId) {
         View view = views.get(viewId);
-        if (view == null){
+        if (view == null) {
             view = getItemView().findViewById(viewId);
             views.put(viewId, view);
         }
     }
 
-    public <V extends View> V getView(int viewId){
+    public <V extends View> V getView(int viewId) {
         return retrieveView(viewId);
     }
 
-    public T setText(int viewId, CharSequence value){
+    public T setText(int viewId, CharSequence value) {
         TextView view = retrieveView(viewId);
         view.setText(value);
         return (T) this;
     }
 
-    public T setText(int viewId, @StringRes int stringRes){
+    public T setText(int viewId, @StringRes int stringRes) {
         TextView view = retrieveView(viewId);
         view.setText(stringRes);
         return (T) this;
     }
 
-    public T setEnable(int viewId, boolean isEnable){
+    public T setEnable(int viewId, boolean isEnable) {
         TextView view = retrieveView(viewId);
         view.setEnabled(isEnable);
         return (T) this;
     }
 
-    public T setImageResource(int viewId, @DrawableRes int imageRes){
+    public T setImageResource(int viewId, @DrawableRes int imageRes) {
         ImageView view = retrieveView(viewId);
         GlideUtil.loadImage(imageRes, view);
         return (T) this;
     }
+
+    public T setBackgroundColor(int viewId, @ColorInt int color) {
+        View view = retrieveView(viewId);
+        view.setBackgroundColor(color);
+        return (T) this;
+    }
+
+    public T setBackgroundRes(int viewId, @DrawableRes int backgroundRes) {
+        View view = retrieveView(viewId);
+        view.setBackgroundResource(backgroundRes);
+        return (T) this;
+    }
+
+    public T setTextColor(int viewId, @ColorInt int textColor) {
+        TextView view = retrieveView(viewId);
+        view.setTextColor(textColor);
+        return (T) this;
+    }
+
+    public T setTextColorRes(int viewId, @ColorRes int textColorRes) {
+        TextView view = retrieveView(viewId);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            view.setTextColor(getItemView().getContext().getResources().getColor(textColorRes, null));
+        } else {
+            view.setTextColor(getItemView().getContext().getResources().getColor(textColorRes));
+        }
+        return (T) this;
+    }
+
 
 }
