d782fc0d6b15a86f862a253fd586bb2d778e0513
lp-pp <1049345904@qq.com>
2017-08-30 18:21:03 +0800

    update

--- app/src/main/java/com/lp/gameclient/adapter/common/BaseAdapterHelper.java
+++ app/src/main/java/com/lp/gameclient/adapter/common/BaseAdapterHelper.java
@@ -14,6 +14,7 @@ import android.text.util.Linkify;
 import android.util.SparseArray;
 import android.view.View;
 import android.view.animation.AlphaAnimation;
+import android.widget.Adapter;
 import android.widget.AdapterView;
 import android.widget.Checkable;
 import android.widget.CheckedTextView;
