5bd06da335e6af9e5e39c94d3f23f6e9636484fe
lp-pp <1049345904@qq.com>
2017-08-31 19:25:17 +0800

    update

--- app/src/main/java/com/lp/gameclient/adapter/common/BaseRecyclerAdapter.java
+++ app/src/main/java/com/lp/gameclient/adapter/common/BaseRecyclerAdapter.java
@@ -2,17 +2,19 @@ package com.lp.gameclient.adapter.common;
 
 import android.content.Context;
 import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.RecyclerView.ViewHolder;
 import android.view.LayoutInflater;
+import android.view.ViewGroup;
 
 import java.util.ArrayList;
 import java.util.List;
 
 /**
- * Created by LP on 2017/8/21/19:57.
+ * Created by LP on 2017/8/31/19:24.
  */
 
 public abstract class BaseRecyclerAdapter<T, H extends RecyclerAdatpterHelper>
-        extends RecyclerView.Adapter<RecyclerView.ViewHolder> implements DataIO<T>{
+        extends RecyclerView.Adapter<ViewHolder> implements DataIO<T>{
 
     protected final Context context;
     protected final int[] layoutResIds;
@@ -30,6 +32,30 @@ public abstract class BaseRecyclerAdapter<T, H extends RecyclerAdatpterHelper>
         this.data = data == null ? new ArrayList<T>() : new ArrayList(data);
     }
 
+    @Override
+    public int getItemViewType(int position) {
+        if (getViewTypeCount() == 1)
+            return super.getItemViewType(position);
+        throw new RuntimeException("Required method getItemViewType was not overridden");
+    }
+
+    public int getViewTypeCount() {
+        return layoutResIds.length;
+    }
+
+    public int getLayoutresId(int viewType){
+        throw new RuntimeException("Required method getLayoutresId was not overridden");
+    }
+
+    @Override
+    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        int layoutResId;
+        if (getViewTypeCount() > 1)
+            layoutResId = getLayoutresId(getItemViewType(viewType));
+        else
+            layoutResId = layoutResIds[0];
+        return new ViewHolder(layoutInflater.inflate(layoutResId, parent, false)){};
+    }
 
 
 }
--- app/src/main/java/com/lp/gameclient/adapter/common/RecyclerAdatpterHelper.java
+++ app/src/main/java/com/lp/gameclient/adapter/common/RecyclerAdatpterHelper.java
@@ -1,10 +1,62 @@
 package com.lp.gameclient.adapter.common;
 
+import android.support.v7.widget.RecyclerView.ViewHolder;
+import android.util.SparseArray;
+import android.view.View;
+
+import com.lp.gamenewsdemo.R;
+
 /**
- * Created by LP on 2017/8/17/19:13.
+ * Created by LP on 2017/8/31/19:25.
  */
 
 final public class RecyclerAdatpterHelper extends BaseAdapterHelper<RecyclerAdatpterHelper>{
 
+    protected ViewHolder viewHolder;
 
+    private RecyclerAdatpterHelper(ViewHolder viewHolder){
+        this.viewHolder = viewHolder;
+        this.views = new SparseArray<>();
+    }
+
+    static RecyclerAdatpterHelper get(ViewHolder viewHolder){
+        RecyclerAdatpterHelper helper;
+        if (viewHolder.itemView.getTag(R.id.tag_adapter_helper) == null){
+            helper = new RecyclerAdatpterHelper(viewHolder);
+            viewHolder.itemView.setTag(R.id.tag_adapter_helper, helper);
+        } else {
+            helper = (RecyclerAdatpterHelper) viewHolder.itemView.getTag(R.id.tag_adapter_helper);
+        }
+        return helper;
+    }
+
+    @Override
+    public View getItemView() {
+        return viewHolder.itemView;
+    }
+
+    public int getItemViewType(){
+        return viewHolder.getItemViewType();
+    }
+
+    public int getAdapterPosition(){
+        return viewHolder.getAdapterPosition();
+    }
+
+    public int getLayoutPosition(){
+        return viewHolder.getLayoutPosition();
+    }
+
+    public int getOldPosition(){
+        return viewHolder.getOldPosition();
+    }
+
+    public boolean isRecyclable(){
+        return viewHolder.isRecyclable();
+    }
+
+    public RecyclerAdatpterHelper setIsRecyclable(boolean recyclable){
+        viewHolder.setIsRecyclable(recyclable);
+        return this;
+    }
 }
