7f275ae6b247fc120581ebf6e6f90f2201805b88
lp-pp <1049345904@qq.com>
2017-09-01 12:14:27 +0800

    update

--- app/src/main/java/com/lp/gameclient/adapter/common/BaseRecyclerAdapter.java
+++ app/src/main/java/com/lp/gameclient/adapter/common/BaseRecyclerAdapter.java
@@ -57,5 +57,136 @@ public abstract class BaseRecyclerAdapter<T, H extends RecyclerAdatpterHelper>
         return new ViewHolder(layoutInflater.inflate(layoutResId, parent, false)){};
     }
 
+    @Override
+    public void onBindViewHolder(ViewHolder holder, int position) {
+        H helper = getAdapterHelper(holder);
+        T item = get(position);
+        convert(helper, item);
+    }
 
+    @Override
+    public int getItemCount() {
+        return getSize();
+    }
+
+    @Override
+    public void add(T elem) {
+        data.add(elem);
+        notifyDataSetChanged();
+        onDataSetChanged();
+    }
+
+    @Override
+    public void addAt(int location, T elem) {
+        data.add(location, elem);
+        notifyDataSetChanged();
+        onDataSetChanged();
+    }
+
+    @Override
+    public void addAll(List<T> elements) {
+        data.addAll(elements);
+        notifyDataSetChanged();
+        onDataSetChanged();
+    }
+
+    @Override
+    public void addAllAt(int location, List<T> elements) {
+        data.addAll(location, elements);
+        notifyDataSetChanged();
+        onDataSetChanged();
+    }
+
+    @Override
+    public void remove(T elem) {
+        data.remove(elem);
+        notifyDataSetChanged();
+        onDataSetChanged();
+    }
+
+    @Override
+    public void removeAll(List<T> elements) {
+        data.removeAll(elements);
+        notifyDataSetChanged();
+        onDataSetChanged();
+    }
+
+    @Override
+    public void removeAt(int index) {
+        data.remove(index);
+        notifyDataSetChanged();
+        onDataSetChanged();
+    }
+
+    @Override
+    public void clear() {
+        if (data != null && data.size() > 0){
+            data.clear();
+            notifyDataSetChanged();
+            onDataSetChanged();
+        }
+    }
+
+    @Override
+    public void replace(T oldElem, T newElem) {
+        replaceAt(data.indexOf(oldElem), newElem);
+    }
+
+    @Override
+    public void replaceAt(int index, T elem) {
+        data.set(index, elem);
+        notifyDataSetChanged();
+    }
+
+    @Override
+    public void replaceAll(List<T> elements) {
+        if (data.size() > 0)
+            data.clear();
+        data.addAll(elements);
+        notifyDataSetChanged();
+        onDataSetChanged();
+    }
+
+    @Override
+    public T get(int position) {
+        if (position >= data.size())
+            return null;
+        return data.get(position);
+    }
+
+    @Override
+    public ArrayList<T> getAll() {
+        return data;
+    }
+
+    @Override
+    public int getSize() {
+        return data.size();
+    }
+
+    @Override
+    public boolean contains(T elem) {
+        return data.contains(elem);
+    }
+
+    private void onDataSetChanged() {
+        if (getSize() == 0)
+            onEmptyData();
+        else
+            onHasData();
+    }
+
+    @Override
+    public void onEmptyData() {
+
+    }
+
+    @Override
+    public void onHasData() {
+
+    }
+
+    protected abstract void convert(H helper, T item);
+
+    public abstract H getAdapterHelper(ViewHolder viewHolder);
 }
