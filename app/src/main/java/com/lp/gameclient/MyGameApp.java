3bb3c3f052e8ba4c371b02b1da40000aca5df14d
lp-pp <1049345904@qq.com>
2017-09-12 18:22:29 +0800

    update

--- app/src/main/java/com/lp/gameclient/MyGameApp.java
+++ app/src/main/java/com/lp/gameclient/MyGameApp.java
@@ -3,7 +3,7 @@ package com.lp.gameclient;
 import android.app.Application;
 
 /**
- * Created by LP on 2017/8/1/19:28.
+ * Created by LP on 2017/9/12/14:30.
  */
 
 public class MyGameApp extends Application {
